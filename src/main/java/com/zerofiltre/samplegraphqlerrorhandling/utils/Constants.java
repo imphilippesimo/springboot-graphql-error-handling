488665aebed0b3e482fd21ab4d3d9522373d4c6c
Alexkouasseu <ktacenterprise@gmail.com>
2020-05-17 18:42:41 +0100

    COnfig Dockerfile deploiement.yaml and microservice.yaml

--- src/main/java/com/zerofiltre/samplegraphqlerrorhandling/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.zerofiltre.samplegraphqlerrorhandling.utils;
-
-public class Constants {
-
-    public static final String TEST_USERNAME = "johndoe";
-    public static final String TEST_PASSWORD = "mypassword";
-}
